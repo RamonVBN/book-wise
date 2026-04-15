fa68a7c3ce86f0cb31b9eacddc807a05b68329cd
RamonVBN <ramon.vitor0106@gmail.com>
2026-04-15 08:30:10 -0300

    fix: error when trying to load specific google books covers

--- src/components/BookCover/index.tsx
+++ src/components/BookCover/index.tsx
@@ -1,16 +1,14 @@
-
-import { useState } from "react"
-import { CoverContainer, Skeleton, StyledImage } from "./styles"
+import { useState } from "react";
+import { CoverContainer, Skeleton, StyledImage } from "./styles";
 
 type BookCoverProps = {
-  src?: string
-  alt?: string
-  sizes?: string
-  priority?: boolean
-  width: number,
-  height: number
-}
-
+  src?: string;
+  alt?: string;
+  sizes?: string;
+  priority?: boolean;
+  width: number;
+  height: number;
+};
 
 export function BookCover({
   src,
@@ -18,39 +16,40 @@ export function BookCover({
   sizes = "100vw",
   priority = false,
   width,
-  height
+  height,
 }: BookCoverProps) {
-  const [loaded, setLoaded] = useState(false)
-  const [error, setError] = useState(false)
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
 
-  const fallback = "/book-placeholder.png"
+  const fallback = "/book-placeholder.png";
 
   return (
     <CoverContainer
-    css={{
+      css={{
         $$width: `${width}px`,
-        $$height: `${height}px`
-    }}
+        $$height: `${height}px`,
+      }}
     >
       {!loaded && <Skeleton />}
 
       <StyledImage
-      css={{
-        $$width: width,
-        $$height: height
-      }}
+        css={{
+          $$width: width,
+          $$height: height,
+        }}
         quality={100}
         key={src}
         width={width}
         height={height}
-        src={!error && src ? src : fallback}
+        src={(!error && src) ? src : fallback}
         alt={alt}
         sizes={sizes}
         priority={priority}
         loaded={loaded}
         onLoadingComplete={() => setLoaded(true)}
         onError={() => setError(true)}
+        unoptimized
       />
     </CoverContainer>
-  )
+  );
 }
