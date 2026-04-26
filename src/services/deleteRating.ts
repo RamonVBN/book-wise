793f81c19d92cd20bfc478644addc60aae271f25
RamonVBN <ramon.vitor0106@gmail.com>
2026-04-26 13:14:45 -0300

    fix: updatedAt column update

--- src/services/deleteRating.ts
+++ src/services/deleteRating.ts
@@ -1,75 +1,70 @@
 import { prisma } from "@/lib/prisma";
 
 type DeleteRatingProps = {
-    userId: string
-    ratingId: string
-    bookId: string,
-    rate: number
-}
+  userId: string;
+  ratingId: string;
+  bookId: string;
+  rate: number;
+};
 
-export default async function deleteRating(
-    {userId, ratingId, bookId, rate
-}: DeleteRatingProps){
+export default async function deleteRating({
+  userId,
+  ratingId,
+  bookId,
+  rate,
+}: DeleteRatingProps) {
+  await prisma.rating.delete({
+    where: {
+      id: ratingId,
+      user: {
+        id: userId,
+      },
+    },
+  });
 
-    await prisma.rating.delete({
-        where: {
-            id: ratingId,
-            user: {
-                id: userId
-            }
-        }
-    })
+  await prisma.$queryRaw`
+  UPDATE "user_books"
+  SET "rated" = false
+  WHERE "user_id" = ${userId}
+    AND "book_id" = ${bookId};
+`;
 
-    await prisma.userBook.update({
-        where: {
-            userId_bookId: {
-                bookId,
-                userId
-            },
-        },
-        data: {
-            rated: false
-        }
-    })
+  const book = await prisma.book.findUnique({
+    where: {
+      id: bookId,
+    },
+    include: {
+      ratings: true,
+    },
+  });
 
-    const book = await prisma.book.findUnique({
-        where: {
-            id: bookId
-        },
-        include: {
-            ratings: true
-        }
-    })
+  if (book!.ratings.length < 1) {
+    await prisma.book.update({
+      where: {
+        id: bookId,
+      },
+      data: {
+        ratingsCount: 0,
+        ratingsSum: 0,
+        avgRating: 0,
+      },
+    });
+  } else {
+    const newRatingsCount = book!.ratingsCount - 1;
+    const newRatingsSum = book!.ratingsSum - rate;
+    const newAvg = newRatingsSum / newRatingsCount;
 
-    if (book!.ratings.length < 1){
+    await prisma.book.update({
+      where: {
+        id: bookId,
+      },
+      data: {
+        ratingsCount: newRatingsCount,
+        ratingsSum: newRatingsSum,
+        avgRating: newAvg,
+      },
+    });
+  }
 
-        await prisma.book.update({
-            where: {
-                id: bookId
-            },
-            data: {
-                ratingsCount: 0,
-                ratingsSum: 0,
-                avgRating: 0
-            } 
-        })
-    } else {
-
-        const newRatingsCount = book!.ratingsCount - 1
-        const newRatingsSum = book!.ratingsSum - rate
-        const newAvg = newRatingsSum / newRatingsCount
-
-        await prisma.book.update({
-            where: {
-                id: bookId
-            },
-            data: {
-                ratingsCount: newRatingsCount,
-                ratingsSum: newRatingsSum,
-                avgRating: newAvg
-            } 
-        })
-    }
-
-    return 
+  return;
 }
