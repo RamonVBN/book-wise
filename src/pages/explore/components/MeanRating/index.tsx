d2bd88b2b5a7518214090d6c19ef5acb3fde7fd0
RamonVBN <ramon.vitor0106@gmail.com>
2026-05-04 11:04:55 -0300

    fix: format book  avg rating

--- src/pages/explore/components/MeanRating/index.tsx
+++ src/pages/explore/components/MeanRating/index.tsx
@@ -1,16 +1,20 @@
-import { MeanRatingContainer, MeanRatingAmount } from "./styles";
+import { MeanRatingContainer, MeanRatingAmount } from "./styles"
 
 type MeanRatingProps = {
-    avgRating: number
+  avgRating: number
 }
 
 export function MeanRating({ avgRating }: MeanRatingProps) {
+  function formatAvgRating(rating: number): string {
+    if (rating.toString().split(".")[1]?.length > 2) {
+      return rating.toFixed(2).toString()
+    }
+    return rating.toString()
+  }
 
-    return (
-        <MeanRatingContainer>
-            <MeanRatingAmount>
-                {avgRating.toFixed(2).toString()}
-            </MeanRatingAmount>
-        </MeanRatingContainer>
-    )
+  return (
+    <MeanRatingContainer>
+      <MeanRatingAmount>{formatAvgRating(avgRating)}</MeanRatingAmount>
+    </MeanRatingContainer>
+  )
 }
