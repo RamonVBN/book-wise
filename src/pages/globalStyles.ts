b40b2b01d8ae6e4bb148344dac9bdf3570a81910
RamonVBN <ramon.vitor0106@gmail.com>
2026-05-04 10:53:52 -0300

    style: elements focus outline

--- src/pages/globalStyles.ts
+++ src/pages/globalStyles.ts
@@ -1,96 +1,100 @@
-import { createStitches } from '@stitches/react';
+import { createStitches } from "@stitches/react"
 
 export const { styled, getCssText, globalCss } = createStitches({
   theme: {
     fonts: {
-      default: 'Nunito, sans-serif',
+      default: "Nunito, sans-serif",
     },
 
-		space: {
-      px: '1px',
-      1: '0.25rem',
-      2: '0.5rem',
-      3: '0.75rem',
-      4: '1rem',
-      5: '1.25rem',
-      6: '1.5rem',
-      7: '1.75rem',
-      8: '2rem',
-      10: '2.5rem',
+    space: {
+      px: "1px",
+      1: "0.25rem",
+      2: "0.5rem",
+      3: "0.75rem",
+      4: "1rem",
+      5: "1.25rem",
+      6: "1.5rem",
+      7: "1.75rem",
+      8: "2rem",
+      10: "2.5rem",
     },
 
-
-	fontWeights: {
-      regular: '400',
-      medium: '500',
-      bold: '700',
+    fontWeights: {
+      regular: "400",
+      medium: "500",
+      bold: "700",
     },
 
     lineHeights: {
-      shorter: '125%',
-      short: '140%',
-      base: '160%',
-      tall: '180%',
+      shorter: "125%",
+      short: "140%",
+      base: "160%",
+      tall: "180%",
     },
 
     colors: {
-	    white: '#FFFFFF',
-      black: '#000000',
+      white: "#FFFFFF",
+      black: "#000000",
 
-      green100: '#50B2C0',
-      green200: '#255D6A',
-      green300: '#0A313C',
+      green100: "#50B2C0",
+      green200: "#255D6A",
+      green300: "#0A313C",
 
-      purple100: '#8381D9',
-      purple200: '#2A2879',
+      purple100: "#8381D9",
+      purple200: "#2A2879",
 
-      gray100: '#F8F9FC',
-      gray200: '#E6E8F2',
-      gray300: '#D1D6E4',
-      gray400: '#8D95AF',
-      gray500: '#303F73',
-      gray600: '#252D4A',
-      gray700: '#181C2A',
-      gray800: '#0E1116',
+      gray100: "#F8F9FC",
+      gray200: "#E6E8F2",
+      gray300: "#D1D6E4",
+      gray400: "#8D95AF",
+      gray500: "#303F73",
+      gray600: "#252D4A",
+      gray700: "#181C2A",
+      gray800: "#0E1116",
 
-      blue100: '#1976D2',
-      pink100: '#FF90C0',
-      red100: '#F75A68',
-      red200: '#9C0012',
-      green400: '#3DAC78',
-      yellow100: '#DBA529',
+      blue100: "#1976D2",
+      pink100: "#FF90C0",
+      red100: "#F75A68",
+      red200: "#9C0012",
+      green400: "#3DAC78",
+      yellow100: "#DBA529",
 
-		'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
-      'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
+      "gradient-vertical": `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
+      "gradient-horizontal": `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
     },
 
-	radii: {
-      xs: '2.5px',
-      sm: '5px',
-      md: '10px',
-      lg: '20px',
-      full: '99999px',
+    radii: {
+      xs: "2.5px",
+      sm: "5px",
+      md: "10px",
+      lg: "20px",
+      full: "99999px",
     },
   },
 })
 
 export const globalStyles = globalCss({
+  html: {
+    overflowX: "hidden",
+  },
 
-    html: {
-      overflowX: 'hidden'
-    },
+  body: {
+    overflowX: "hidden",
+  },
 
-    body: {
-      overflowX: 'hidden'
-    },
+  "*:focus": {
+    outline: "none",
+  },
+  "*:focus-visible": {
+    outline: "2px solid $primary",
+  },
 
-    '*': {
-        margin: 0,
-        padding: 0,
-        boxSizing: 'border-box',
-        fontFamily: '$default',  
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+    fontFamily: "$default",
 
-        '-webkit-font-smooting': 'antialiased',
-
-    },
+    "-webkit-font-smooting": "antialiased",
+  },
 })
